a10495f4930437a866458c00b9186d92f54d1fc5
nii <juniorAkwei2@gmail.com>
2025-05-18 19:48:30 +0000

    update cloudinary

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
-import cron from "node-cron";
 import { setDb } from "./config/db.js";
 import { router as authRouter } from "./routes/auth.route.js";
 import { router as productRouter } from "./routes/product.route.js";
