342f55fe2c59cc49f74469f4effe8ef4efc8cb35
nii <juniorAkwei2@gmail.com>
2025-05-18 19:40:22 +0000

    implemented cron to keep server active

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import cron from "node-cron";
 import { setDb } from "./config/db.js";
 import { router as authRouter } from "./routes/auth.route.js";
 import { router as productRouter } from "./routes/product.route.js";
 import { router as categoryRouter } from "./routes/category.route.js";
 import { router as cartRouter } from "./routes/cart.route.js";
+import job from "./lib/cron.js";
 import morgan from "morgan";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+job.start();
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
+
 app.use("/api", authRouter);
 app.use("/api", productRouter);
 app.use("/api", categoryRouter);
