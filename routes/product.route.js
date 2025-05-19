07a9d412eab84ac02e88317ba2f3273d94a5c960
nii <juniorAkwei2@gmail.com>
2025-05-19 13:15:33 +0000

    new product routes

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,6 +1,13 @@
 import express from "express";
-import { createProduct, upload } from "../controllers/product.js";
+import {
+  createProduct,
+  getNewProducts,
+  getTopSellingProducts,
+  upload,
+} from "../controllers/product.js";
 
 export const router = express.Router();
 
 router.post("/upload", upload.single("image"), createProduct);
+router.get("/new-product", getNewProducts);
+router.get("/top-selling-product", getTopSellingProducts);
